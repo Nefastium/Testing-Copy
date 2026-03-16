b7412567818212de3f7d96fa517d33596e375a37
Nefastium <marinkovichezequiel@gmail.com>
2026-03-16 19:45:55 -0300

    fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "var(--color-border)",
+        background: "var(--color-background)",
+        foreground: "var(--color-foreground)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        muted: "var(--color-muted)",
+      },
+    },
   },
   plugins: [],
 }
